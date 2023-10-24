69c8305b7d9f284d1d2a7747d644a4e67f65a08c
Sara Chronister <57767797+sara-chronister@users.noreply.github.com>
2023-10-24 11:58:44 -0700

    Add files via upload

---
+++ Scripts/COVIDprep.R
@@ -0,0 +1,19 @@
+covid_df <- group_resp_data(resp_percents, condition = "COVID") %>%
+  filter(Year>=2020) %>%
+  mutate(drop = case_when(
+    Year==2021 & Week >= 26 ~ TRUE,
+    Year==2022 & Week <=12 ~ TRUE
+  ),
+  percent = COVID*100) %>%
+  filter(is.na(drop)) %>%
+  select(Year, Week, percent)
+write.csv(covid_df,file = "Data/covid_df.csv",
+          row.names = FALSE)
+## USE MEM SHINY APP TO DETERMINE THRESHOLDS AND FILL IN BELOW
+covid_thresholds <- data.frame(
+  Condition = "COVID",
+  epidemic = .0376,
+  medium = .0393, 
+  high = .0805, 
+  veryhigh = .1165)
+save(covid_thresholds, file = "Data/covid_thresholds.RData")
